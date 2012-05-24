075d257c23cb5d9cbfa097f95cb0cf43e0f7668c
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2012-05-24 13:57:24 +0000

    - Adding template plugin - Moving Get...Suffix and Get...Tag functions to separate file - Adding bridge/util.h with functions for easier registration

---
+++ template_plugin.cpp
@@ -0,0 +1,132 @@
+/**
+ * Template plugin
+ */
+
+#include "bridge/util.h"
+
+using namespace std;
+using namespace ug::bridge;
+
+namespace ug{
+namespace Template{
+
+/**
+ * Class exporting the functionality of the plugin. All functionality that is to
+ * be used in scripts or visualization must be registered here.
+ */
+struct Functionality
+{
+
+/**
+ * Function called for the registration of Domain and Algebra dependent parts
+ * of the plugin. All Functions and Classes depending on both Domain and Algebra
+ * are to be placed here when registering. The method is called for all
+ * available Domain and Algebra types, based on the current build options.
+ *
+ * @param reg				registry
+ * @param parentGroup		group for sorting of functionality
+ */
+template <typename TDomain, typename TAlgebra>
+static void DomainAlgebra(Registry& reg, string parentGroup)
+{
+//	useful defines
+	string suffix = GetDomainAlgebraSuffix<TDomain,TAlgebra>();
+	string tag = GetDomainAlgebraTag<TDomain,TAlgebra>();
+
+}
+
+/**
+ * Function called for the registration of Domain dependent parts
+ * of the plugin. All Functions and Classes depending on the Domain
+ * are to be placed here when registering. The method is called for all
+ * available Domain types, based on the current build options.
+ *
+ * @param reg				registry
+ * @param parentGroup		group for sorting of functionality
+ */
+template <typename TDomain>
+static void Domain(Registry& reg, string grp)
+{
+//	useful defines
+	string suffix = GetDomainSuffix<TDomain>();
+	string tag = GetDomainTag<TDomain>();
+
+}
+
+/**
+ * Function called for the registration of Dimension dependent parts
+ * of the plugin. All Functions and Classes depending on the Dimension
+ * are to be placed here when registering. The method is called for all
+ * available Dimension types, based on the current build options.
+ *
+ * @param reg				registry
+ * @param parentGroup		group for sorting of functionality
+ */
+template <int dim>
+static void Dimension(Registry& reg, string grp)
+{
+//	useful defines
+	string suffix = GetDimensionSuffix<dim>();
+	string tag = GetDimensionTag<dim>();
+
+}
+
+/**
+ * Function called for the registration of Algebra dependent parts
+ * of the plugin. All Functions and Classes depending on Algebra
+ * are to be placed here when registering. The method is called for all
+ * available Algebra types, based on the current build options.
+ *
+ * @param reg				registry
+ * @param parentGroup		group for sorting of functionality
+ */
+template <typename TAlgebra>
+static void Algebra(Registry& reg, string parentGroup)
+{
+//	useful defines
+	string suffix = GetAlgebraSuffix<TAlgebra>();
+	string tag = GetAlgebraTag<TAlgebra>();
+
+}
+
+/**
+ * Function called for the registration of Domain and Algebra independent parts
+ * of the plugin. All Functions and Classes not depending on Domain and Algebra
+ * are to be placed here when registering.
+ *
+ * @param reg				registry
+ * @param parentGroup		group for sorting of functionality
+ */
+static void Common(Registry& reg, string parentGroup)
+{
+
+}
+
+}; // end Functionality
+} // end namespace Template
+
+
+/**
+ * This function is called when the plugin is loaded.
+ */
+extern "C" void
+InitUGPlugin_Template(Registry* reg, string parentGroup)
+{
+	string grp(parentGroup); grp.append("Template/");
+	typedef Template::Functionality Functionality;
+
+	try{
+		RegisterCommon<Functionality>(*reg,grp);
+		RegisterDimensionDependent<Functionality>(*reg,grp);
+		RegisterDomainDependent<Functionality>(*reg,grp);
+		RegisterAlgebraDependent<Functionality>(*reg,grp);
+		RegisterDomainAlgebraDependent<Functionality>(*reg,grp);
+	}
+	catch(UG_REGISTRY_ERROR_RegistrationFailed ex)
+	{
+		UG_LOG("\n### ERROR in plugin: "<<grp<<". "
+				"Registration failed (using name " << ex.name << ").\n");
+	}
+}
+
+}// namespace ug
